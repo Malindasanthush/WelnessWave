035efbfbf556dfa2e08af73263c0fd77af705168
Malinda Santhush <168289984+Malindasanthush@users.noreply.github.com>
2024-10-23 15:13:33 +0530

    WelnessWave/Database

---
+++ new.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307
+-- Generation Time: Oct 23, 2024 at 01:06 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `new`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `feedback` text NOT NULL,
+  `rating` int(11) NOT NULL CHECK (`rating` >= 1 and `rating` <= 5),
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `name`, `feedback`, `rating`, `created_at`) VALUES
+(1, 'Malinda Santhush', 'good', 3, '2024-10-22 20:57:33');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userstable`
+--
+
+CREATE TABLE `userstable` (
+  `usersId` int(11) NOT NULL,
+  `usersName` varchar(255) NOT NULL,
+  `usersEmail` varchar(255) NOT NULL,
+  `usersUid` varchar(255) NOT NULL,
+  `usersPassword` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `userstable`
+--
+
+INSERT INTO `userstable` (`usersId`, `usersName`, `usersEmail`, `usersUid`, `usersPassword`) VALUES
+(2, 'Malinda Santhush', 'admin@gmail.com', 'Malinda Santhush', '$2y$10$Omuo8JUENHO4gDDoYPhFeOb9J/PCks0jmWh1ve5UDgEaSs5ZGRS8y');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userstable`
+--
+ALTER TABLE `userstable`
+  ADD PRIMARY KEY (`usersId`),
+  ADD KEY `idx_usersUid` (`usersUid`),
+  ADD KEY `idx_usersEmail` (`usersEmail`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `userstable`
+--
+ALTER TABLE `userstable`
+  MODIFY `usersId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
